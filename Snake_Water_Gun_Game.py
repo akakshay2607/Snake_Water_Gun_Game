ffbc1f2fa1eec4c09aea9603b059941faf0291fa
akakshay2607 <92714663+akakshay2607@users.noreply.github.com>
2022-04-03 22:27:56 +0530

    Update Snake_Water_Gun_Game.py

--- Snake_Water_Gun_Game.py
+++ Snake_Water_Gun_Game.py
@@ -88,13 +88,3 @@ print(f"your point is {human_point} and computer point is {computer_point}")
 #
 # Snake Water Gun Game in Python
 # The snake drinks the water, the gun shoots the snake, and gun has no effect on water.
-#
-
-  
-
-
-# In[ ]:
-
-
-
-
